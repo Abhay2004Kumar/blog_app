729e90dafa9f00b866535491c7f7143fbfbe55c8
Abhay2004Kumar <abs220502@gmail.com>
2024-12-18 15:51:04 +0530

    add components for react-hook form and AuthLayout

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,7 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container"
 import Logo from "./Logo";
 import LogOutBtn from "./Header/LogOutBtn";
+import Button from './Button'
+import Input from './Input'
 
-export {Header, Footer,Container, Logo, LogOutBtn}
+export {Header, Footer,Container, Logo, LogOutBtn,Button, Input}
