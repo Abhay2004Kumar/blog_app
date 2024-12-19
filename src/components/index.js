985dd719944ed1d74d3e042dac70205f7b65888b
Abhay2004Kumar <abs220502@gmail.com>
2024-12-19 14:55:40 +0530

    added pages

--- src/components/index.js
+++ src/components/index.js
@@ -7,5 +7,9 @@ import Button from './Button'
 import Input from './Input'
 import Select from './Select'
 import RTE from "./RTE";
-
-export {Header, Footer,Container, Logo, LogOutBtn,Button, Input, Select, RTE}
+import SignUp from "./SignUp"
+import Login from "./Login"
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout" 
+export {Header, Footer,Container, Logo, LogOutBtn,Button, Input, Select, RTE,SignUp, Login,PostForm, PostCard, AuthLayout}
---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SignUp as SignUpComponenet } from '../components'
+
+function SignUpPage() {
+  return (
+   <div className='py-8'>
+<SignUpComponenet/>
+   </div>
+  )
+}
+
+export default SignUpPage
